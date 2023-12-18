bfd9d1791ed8187d8419d04a3ed8925023408701
Albert <testingpon@gmail.com>
2023-12-18 23:28:52 +0600

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,13 +5,40 @@ import css from './Footer.module.scss'
 
 import Logo from '../../assets/svg/Logo.svg'
 
-// ---------------------------------------------------
+/// ---------------------------------------------------
+// Componets
+
+import Button from '../UI/Button/Button'
+
+/// ---------------------------------------------------
 const Footer = () => {
 	return (
 		<footer className={css.Footer}>
 			<div className='container'>
-        <div className={css.footer}></div>
-      </div>
+				<div className={css.footer}>
+					<div>
+						<img src={Logo} alt='Logo' />
+					</div>
+					<ol>
+						<li>Главная</li>
+						<li>Каталог</li>
+						<li>О компании</li>
+						<li>Контакты</li>
+					</ol>
+					<div className={css.footer__phone}>
+						<p>8 800 111-11-11</p>
+						<p>с 9:00 - до 18:00</p>
+					</div>
+					<div className={css.footer__button}>
+						<Button>Регистрация</Button>
+					</div>
+				</div>
+				<div className={css.footer__line}></div>
+				<div className={css.footer__under}>
+					<p>© Bassco 2022</p>
+					<p>Политика конфиденциальности</p>
+				</div>
+			</div>
 		</footer>
 	)
 }
